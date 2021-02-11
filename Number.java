b444c7289ccd7ff206b0990a3658b022ac731e2f
Jacob Merioles <600021653@fjuhsd.org>
2021-02-11 02:19:58 +0000

    finished Unit 7 Lab 4

--- Number.java
+++ Number.java
@@ -1,28 +1,45 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jacob Merioles
 
 
-//write the Number class
-//use the handout and
-//sample runner code
-//to guide you
+//write the Number class; use the handout and sample runner code to guide you
 
 //header for Number class
+public class Number {
+  //private instance variable (must be an Integer)
+  private Integer x;
+  //constructor
+  public Number(int num) {
+    x = num;
+  }
 
-//private instance variable (must be an Integer)
+  //getter method
+  public Integer getNumber(){
+    return x;
+  }
 
-//constructor
+  //setter method
+  public void setNumber(int y){
+    x = y;
+  }
+  //isOdd method
+  public boolean isOdd(){
+    return (x % 2 != 0);
+  }
+  //isPerfect method
+  public boolean isPerfect(){
+    int sum = 0;
+    for (int i = 1; i < x; i++){
+      if (x % i == 0)
+        sum += i;
+    }
+    return (sum == x);
+  }
 
+  //toString
+  public String toString(){
+    return "" + x;
+  }
 
-//getter method
-
-
-//setter method
-
-//isOdd method
-
-//isPerfect method
-
-
-//toString
+} 
